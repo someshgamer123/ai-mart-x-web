c1b4d9fd91307545f4395f3c341424e3bd3dea4d
Somesh <mikunkumar242@gmail.com>
2026-09-05 23:15:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# config.py
 import os
 from dotenv import load_dotenv
 
@@ -5,14 +6,19 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "martx_super_secure_session_key_2026")
-    MONGO_URI = mongodb+srv://mikunkumar242_db_user:<somesh63723348>@cluster0.huo0uug.mongodb.net/?appName=Cluster0
+    
+    # Aapka MongoDB Connection URI
+    MONGO_URI = os.getenv(
+        "MONGO_URI", 
+        "mongodb+srv://mikunkumar242_db_user:somesh63723348@cluster0.huo0uug.mongodb.net/?appName=Cluster0"
+    )
+    
+    DB_NAME = os.getenv("DB_NAME", "mart_x_web_db")
     
     # Admin Credentials
     ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "someshkumarbiswaliamsomu@gmail.com")
-    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "SoMuAdMiN@hsh6474764")  # Change in production
+    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "SoMuAdMiN@hsh6474764")
     
-    # Defaults
+    # Store Economics
     DEFAULT_COINS_PER_INR = os.getenv("DEFAULT_COINS_PER_INR", "10")
-    DEFAULT_ADMIN_UPI = os.getenv("DEFAULT_ADMIN_UPI", "somuofc@ptyes")
-    SUPPORT_HANDLE = os.getenv("SUPPORT_HANDLE", "SOMU_VERIFIED")
-    WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "not_linked")
+    SUPPORT_HANDLE = os.getenv("SUPPORT_HANDLE", "SOMU_VERIFIED")  # Aapka Telegram Username
