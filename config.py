9ae4c181ef03353e4d1647da35670947eeb2940c
Somesh <mikunkumar242@gmail.com>
2026-09-05 23:31:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "martx_super_secure_session_key_2026")
     
-    # Aapka MongoDB Connection URI
+    # MongoDB Connection URI
     MONGO_URI = os.getenv(
         "MONGO_URI", 
         "mongodb+srv://mikunkumar242_db_user:Somesh93372585sg@cluster0.huo0uug.mongodb.net/?appName=Cluster0"
@@ -19,6 +19,7 @@ class Config:
     ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "someshkumarbiswaliamsomu@gmail.com")
     ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "SoMuAdMiN@hsh6474764")
     
-    # Store Economics
+    # Store Settings
     DEFAULT_COINS_PER_INR = os.getenv("DEFAULT_COINS_PER_INR", "10")
-    SUPPORT_HANDLE = os.getenv("SUPPORT_HANDLE", "SOMU_VERIFIED")  # Aapka Telegram Username
+    DEFAULT_ADMIN_UPI = os.getenv("DEFAULT_ADMIN_UPI", "somuofc@ptyes")
+    SUPPORT_HANDLE = os.getenv("SUPPORT_HANDLE", "SOMU_VERIFIED")
